535061e4dfffa823e7a0aaeaea8c80cb0279ad85
Falnadaf <ferasalnadaf77@gmail.com>
2024-04-21 19:54:01 +0200

    first commit

---
+++ client/src/config/index.js
@@ -0,0 +1,6 @@
+export const BACKEND_URL = "https://BIN5.onrender.com
+
+";
+// export const HOST_URL = 'https://storehouse.onrender.com'
+// export const BACKEND_URL = 'http://localhost:8080'
+export const HOST_URL = "https://gaming-console.netlify.app";

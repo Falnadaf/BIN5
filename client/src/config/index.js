5b8b638e981ac5b4a66c1c3854ea178bb7767f01
Falnadaf <ferasalnadaf77@gmail.com>
2024-04-21 20:07:29 +0200

    Update file with a descriptive message

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,6 +1,4 @@
-export const BACKEND_URL = "https://BIN5.onrender.com
-
-";
+export const BACKEND_URL = "https://bin5.onrender.com";
 // export const HOST_URL = 'https://storehouse.onrender.com'
 // export const BACKEND_URL = 'http://localhost:8080'
 export const HOST_URL = "https://gaming-console.netlify.app";
